8b78079de38808ca77cf5f88a7518d089f435118
juliocesaretec <161508817+juliocesaretec@users.noreply.github.com>
2026-02-10 17:40:54 -0300

    Add files via upload

---
+++ aula01/index.php
@@ -0,0 +1,10 @@
+<?php 
+    $nome = "Ana";
+    $idade = 10;
+    $preco = 10000000;
+    $ativo = true;
+    echo "nome: " . $nome . "<br>";
+    echo "Idade: " . $idade . "<br>";
+    echo "preço: " . $preco . "<br>";
+    echo "status: " . ($ativo ? "Ativo" : "inativo");
+     ?>
